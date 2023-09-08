3a9b4d1b22d48935c43f25c72daa6fe48473f438
cparadac <c.paradac@uniandes.edu.co>
2023-09-07 23:51:42 -0500

    [HU005] Test Validar Ejercicio atributos vacios (Verde)

--- src/logica/LogicaEnForma.py
+++ src/logica/LogicaEnForma.py
@@ -5,3 +5,9 @@ class LogicaEnForma(FachadaEnForma):
 
     def __init__(self):
         ""
+
+    def validar_crear_editar_ejercicio(self, nombre, descripcion, enlace, calorias):
+        if len(nombre) == 0:
+            return "Error, el campo nombre esta vacio"
+
+        return ""
