05448b9d5259e034e2c65e92f46420df47cf2eea
cparadac <c.paradac@uniandes.edu.co>
2023-09-08 02:03:56 -0500

    [HU005] se realiza la implentacion de if - elif para cada una de las validaciones y se deja un unico return (Azul)

--- src/logica/LogicaEnForma.py
+++ src/logica/LogicaEnForma.py
@@ -12,42 +12,42 @@ class LogicaEnForma(FachadaEnForma):
         ""
 
     def validar_crear_editar_ejercicio(self, nombre, descripcion, enlace, calorias):
+        error = ""
+
         if len(nombre) == 0:
-            return "Error, el campo nombre esta vacio"
+            error = "Error, el campo nombre esta vacio"
 
-        if len(nombre) > 40:
-            return "Error, el campo nombre supera los 40 caracteres"
+        elif len(nombre) > 40:
+            error = "Error, el campo nombre supera los 40 caracteres"
 
-        if len(descripcion) == 0:
-            return "Error, el campo descripcion esta vacio"
+        elif len(descripcion) == 0:
+            error = "Error, el campo descripcion esta vacio"
 
-        if len(descripcion) > 250:
-            return "Error, el campo descripcion supera los 250 caracteres"
+        elif len(descripcion) > 250:
+            error = "Error, el campo descripcion supera los 250 caracteres"
 
-        if len(enlace) == 0:
-            return "Error, el campo enlace esta vacio"
+        elif len(enlace) == 0:
+            error = "Error, el campo enlace esta vacio"
 
-        if len(enlace) == 0:
-            return "Error, el campo enlace esta vacio"
+        elif not validators.url(enlace):
+            error = "Error, el campo enlace es incorrecto"
 
-        if not validators.url(enlace):
-            return "Error, el campo enlace es incorrecto"
+        elif "https://www.youtube.com/" not in enlace:
+            error = "Error, el campo enlace no es de Youtube"
 
-        if "https://www.youtube.com/" not in enlace:
-            return "Error, el campo enlace no es de Youtube"
+        elif type(calorias) != int:
+            error = "Error, el campo calorias debe ser un número entero"
 
-        if type(calorias) != int:
-            return "Error, el campo calorias debe ser un número entero"
+        elif calorias <= 0:
+            error = "Error, el campo calorias debe ser mayor a cero"
 
-        if calorias <= 0:
-            return "Error, el campo calorias debe ser mayor a cero"
+        else:
+            busqueda = session.query(Ejercicio).filter(Ejercicio.nombre == nombre).all()
 
-        busqueda = session.query(Ejercicio).filter(Ejercicio.nombre == nombre).all()
+            if len(busqueda) > 0:
+                error = "Error, el ejericio " + nombre + " ya existe"
 
-        if len(busqueda) > 0:
-            return "Error, el ejericio " + nombre + " ya existe"
-
-        return ""
+        return error
 
     def crear_ejercicio(self, nombre, descripcion, enlace, calorias):
         ejercicio = Ejercicio(nombre=nombre, descripcion=descripcion, enlaceYoutube=enlace,
