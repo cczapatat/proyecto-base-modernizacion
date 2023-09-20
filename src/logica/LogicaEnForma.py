161a5d5cae8f3c3434e63e2d24acb117ffa4df79
cparadac <c.paradac@uniandes.edu.co>
2023-09-19 23:33:23 -0500

    esta es una prueba de no correr CI Action

--- src/logica/LogicaEnForma.py
+++ src/logica/LogicaEnForma.py
@@ -44,6 +44,7 @@ class LogicaEnForma(FachadaEnForma):
     def obtener_fecha_de_string(self, str_fecha, formato):
         return datetime.datetime.strptime(str_fecha, formato)
 
+
     def fecha_menor_igual_dia_actual(self, fecha):
         return fecha <= datetime.datetime.now()
 
