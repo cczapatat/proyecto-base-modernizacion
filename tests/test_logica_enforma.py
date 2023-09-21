9799a6d66ee400ad3a21b1d1f8525a0c5c9257c3
cczapatat <c.zapatat@uniandes.edu.co>
2023-09-20 23:38:05 -0500

    [HU006] Test Editar ejericicio (Verde)

--- tests/test_logica_enforma.py
+++ tests/test_logica_enforma.py
@@ -237,47 +237,47 @@ class LogicaEnFormaTestCase(unittest.TestCase):
         return self.session.query(Ejercicio).first()
 
     def test_validar_ejercicio_nombre_vacio(self):
-        resultado = self.logica.validar_crear_editar_ejercicio("", "", "", 0)
+        resultado = self.logica.validar_crear_editar_ejercicio("", "", "", 0, -1)
         self.assertEqual(resultado, "Error, el campo nombre esta vacio")
 
     def test_validar_ejercicio_nombre_superar_longitud(self):
-        resultado = self.logica.validar_crear_editar_ejercicio(41 * "Z", "", "", 0)
+        resultado = self.logica.validar_crear_editar_ejercicio(41 * "Z", "", "", 0, -1)
         self.assertEqual(resultado, "Error, el campo nombre supera los 40 caracteres")
 
     def test_validar_ejercicio_descripcion_vacio(self):
-        resultado = self.logica.validar_crear_editar_ejercicio("Burpies", "", "", 0)
+        resultado = self.logica.validar_crear_editar_ejercicio("Burpies", "", "", 0, 1)
         self.assertEqual(resultado, "Error, el campo descripcion esta vacio")
 
     def test_validar_ejercicio_descripcion_supera_longitud(self):
-        resultado = self.logica.validar_crear_editar_ejercicio("Burpies", 51 * "Salto", "", 0)
+        resultado = self.logica.validar_crear_editar_ejercicio("Burpies", 51 * "Salto", "", 0, 1)
         self.assertEqual(resultado, "Error, el campo descripcion supera los 250 caracteres")
 
     def test_validar_ejercicio_enlace_vacio(self):
-        resultado = self.logica.validar_crear_editar_ejercicio("Burpies", "Salto y Flexion", "", 0)
+        resultado = self.logica.validar_crear_editar_ejercicio("Burpies", "Salto y Flexion", "", 0, -1)
         self.assertEqual(resultado, "Error, el campo enlace esta vacio")
 
     def test_validar_ejercicio_enlace_incorrecto(self):
-        resultado = self.logica.validar_crear_editar_ejercicio("Burpies", "Salto y Flexion", "incorrecto", 0)
+        resultado = self.logica.validar_crear_editar_ejercicio("Burpies", "Salto y Flexion", "incorrecto", 0, 1)
         self.assertEqual(resultado, "Error, el campo enlace es incorrecto")
 
     def test_validar_ejercicio_enlace_no_es_youtube(self):
-        resultado = self.logica.validar_crear_editar_ejercicio("Burpies", "Salto y Flexion", "https://google.com/any",                                                    0)
+        resultado = self.logica.validar_crear_editar_ejercicio("Burpies", "Salto y Flexion", "https://google.com/any", 0, -1)
         self.assertEqual(resultado, "Error, el campo enlace no es de Youtube")
 
     def test_validar_ejercicio_calorias_incorrectas(self):
-        resultado = self.logica.validar_crear_editar_ejercicio("Burpies", "Salto y Flexion",                                                   "https://www.youtube.com/watch?v=bmNGEzHi4-s", "")
+        resultado = self.logica.validar_crear_editar_ejercicio("Burpies", "Salto y Flexion", "https://www.youtube.com/watch?v=bmNGEzHi4-s", "", -1)
         self.assertEqual(resultado, "Error, el campo calorias debe ser un número entero")
 
     def test_validar_ejercicio_calorias_no_es_mayor_a_cero(self):
-        resultado = self.logica.validar_crear_editar_ejercicio("Burpies", "Salto y Flexion",                                                 "https://www.youtube.com/watch?v=bmNGEzHi4-s", -1)
+        resultado = self.logica.validar_crear_editar_ejercicio("Burpies", "Salto y Flexion", "https://www.youtube.com/watch?v=bmNGEzHi4-s", -1, -1)
         self.assertEqual(resultado, "Error, el campo calorias debe ser mayor a cero")
 
     def test_validar_ejercicio_nombre_duplicado(self):
-        resultado = self.logica.validar_crear_editar_ejercicio("Salto Lazo", "Saltar 15 veces",                                                  "https://www.youtube.com/watch?v=bmNGEzHi4-s", 10)
+        resultado = self.logica.validar_crear_editar_ejercicio("Salto Lazo", "Saltar 15 veces", "https://www.youtube.com/watch?v=bmNGEzHi4-s", 10, -1)
         self.assertEqual(resultado, "Error, el ejericio Salto Lazo ya existe")
 
     def test_validar_ejercicio_exitoso(self):
-        resultado = self.logica.validar_crear_editar_ejercicio("Bupies", "Salto y Flexion",                                                   "https://www.youtube.com/watch?v=bmNGEzHi4-s", 10)
+        resultado = self.logica.validar_crear_editar_ejercicio("Bupies", "Salto y Flexion", "https://www.youtube.com/watch?v=bmNGEzHi4-s", 10, -1)
         self.assertEqual(resultado, "")
 
     def test_crear_ejercicio(self):
